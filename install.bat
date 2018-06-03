4f439d042cba7785ad66bd25a6233029c37fbb85
assoune.mike <assoune.mike@gmail.com>
2018-06-03 19:45:29 +0200

    finish

---
+++ install.bat
@@ -0,0 +1,4 @@
+sudo apt-get install build-essential clang libdbus-1-dev libgtk2.0-dev \
+                       libnotify-dev libgnome-keyring-dev libgconf2-dev \
+                       libasound2-dev libcap-dev libcups2-dev libxtst-dev \
+                       libxss1 libnss3-dev gcc-multilib g++-multilib
