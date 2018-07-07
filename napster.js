de0df02cc3c5eaffaa09d7e0c0d726d18dcee01b
ASSOUNE Mike <massoune@nexity.fr>
2018-07-08 01:14:11 +0200

    'sdv'

--- napster.js
+++ napster.js
@@ -143,12 +143,6 @@ const main = async (restart) => {
     }
     catch (e) {
       console.log("error", account, e)
-      const html = await nightmare.evaluate(() => {
-        return $('body').html()
-      })
-      fs.writeFile(login + '.txt', html, function (err) {
-        if (err) return console.log(err);
-      });
       clearInterval(inter)
       await nightmare.end()
       if (account) {
