6fc75f67343d0012f537068b471128a50e59f955
ASSOUNE Mike <massoune@nexity.fr>
2018-08-28 16:08:18 +0200

    c

--- spotify.js
+++ spotify.js
@@ -50,7 +50,7 @@ const anticaptcha = (captchaisNew) => {
         method: 'POST',
         json: true,
         data: {
-          clientKey: '5cf44dee27fed739df49a69bb4494b9a',
+          clientKey: '21648811563096fd1970c47f55b3d548',
           taskId: response.taskId
         }
       }, function (err, res, response) {
@@ -79,212 +79,210 @@ const anticaptcha = (captchaisNew) => {
 
 
 const main = async (restart) => {
-  setTimeout(async () => {
-    let account = accounts.shift()
-    // while (accountsValid.indexOf(account) >= 0) {
-    //   fs.writeFile('spotifyAccount.txt', accountsValid.concat(accounts), function (err) {
-    //     if (err) return console.log(err);
-    //   });
-    //   if (accounts.length) {
-    //     account = accounts.shift()
-    //   }
+  let account = accounts.shift()
+  // while (accountsValid.indexOf(account) >= 0) {
+  //   fs.writeFile('spotifyAccount.txt', accountsValid.concat(accounts), function (err) {
+  //     if (err) return console.log(err);
+  //   });
+  //   if (accounts.length) {
+  //     account = accounts.shift()
+  //   }
+  // }
+  let inter
+  const Nightmare = require('nightmare')
+  const nightmare = Nightmare({
+    electronPath: require('electron'),
+    // openDevTools: {
+    //   mode: 'detach'
+    // },
+    alwaysOnTop: false,
+    waitTimeout: 1000 * 60,
+    show: true,
+    typeInterval: 300,
+    webPreferences: {
+      webSecurity: false,
+      allowRunningInsecureContent: true,
+      plugins: true,
+      experimentalFeatures: true
+    }
+  })
+
+  accountInfo = account.split(':')
+  const player = accountInfo[0]
+  const login = accountInfo[1]
+  const pass = accountInfo[2]
+
+  let inputs = {
+    username: '#username',
+    password: '#password'
+  }
+  let url
+  let loginBtn
+  let playBtn
+
+  url = 'https://accounts.spotify.com/dk/login'
+  loginBtn = '#login-button'
+  playBtn = '.tracklist-play-pause.tracklist-middle-align'
+  inputs.username = 'form input[name="username"]'
+  inputs.password = 'form input[name="password"]'
+
+  try {
+
+    const album = () => albums[rand(albums.length)]
+    let nAl = album()
+
+    const error = await nightmare
+      .goto(url)
+      .wait(4000 + rand(2000))
+      .type(inputs.username, login)
+      .type(inputs.password, pass)
+      .wait(2000)
+      .evaluate((captcha) => {
+        window.___grecaptcha_cfg.clients[0].aa.l.callback(captcha)
+      }, captcha)
+    // .click(loginBtn)
+
+    // const free = await nightmare
+    //   .wait(4000 + rand(2000))
+    //   .goto('https://www.spotify.com/dk/account/overview/')
+    //   .wait('#js-navbar')
+    //   .wait(4000 + rand(2000))
+    //   .evaluate(() => {
+    //     return /free/.test($('.subscription-status').html())
+    //   })
+
+    // if (free) {
+    //   throw 'getout'
     // }
-    let inter
-    const Nightmare = require('nightmare')
-    const nightmare = Nightmare({
-      electronPath: require('electron'),
-      // openDevTools: {
-      //   mode: 'detach'
-      // },
-      alwaysOnTop: false,
-      waitTimeout: 1000 * 60,
-      show: true,
-      typeInterval: 300,
-      webPreferences: {
-        webSecurity: false,
-        allowRunningInsecureContent: true,
-        plugins: true,
-        experimentalFeatures: true
-      }
-    })
 
-    accountInfo = account.split(':')
-    const player = accountInfo[0]
-    const login = accountInfo[1]
-    const pass = accountInfo[2]
-
-    let inputs = {
-      username: '#username',
-      password: '#password'
+    accountsValid.push(account)
+    if (accountsValid.length === 1 || accountsValid.length % 10 === 0) {
+      // console.log('total ' + accountsValid.length)
     }
-    let url
-    let loginBtn
-    let playBtn
 
-    url = 'https://accounts.spotify.com/dk/login'
-    loginBtn = '#login-button'
-    playBtn = '.tracklist-play-pause.tracklist-middle-align'
-    inputs.username = 'form input[name="username"]'
-    inputs.password = 'form input[name="password"]'
+    let noplay = await nightmare
+      .wait(4000 + rand(2000))
+      .goto(nAl)
+      .wait(4000 + rand(2000))
 
-    try {
+    const errorhtml = await nightmare
+      // .click(playBtn)
+      .wait(4000 + rand(2000))
+      .evaluate(() => {
+        let shuffle = '.spoticon-shuffle-16:not(.control-button--active)'
+        let repeat = '.spoticon-repeat-16:not(.control-button--active)'
+        let playBtn = '.tracklist-play-pause.tracklist-middle-align'
 
-      const album = () => albums[rand(albums.length)]
-      let nAl = album()
+        if (!document.querySelector(playBtn)) {
+          return document.querySelector('body').innerHTML
+        }
 
-      const error = await nightmare
-        .goto(url)
-        .wait(4000 + rand(2000))
-        .type(inputs.username, login)
-        .type(inputs.password, pass)
-        .wait(2000)
-        .evaluate((captcha) => {
-          window.___grecaptcha_cfg.clients[0].aa.l.callback(captcha)
-        }, captcha)
-      // .click(loginBtn)
+        document.querySelector(playBtn) && document.querySelector(playBtn).click()
+        setTimeout(() => {
+          document.querySelector(shuffle) && document.querySelector(shuffle).click()
+        }, 1000);
+        setTimeout(() => {
+          document.querySelector(repeat) && document.querySelector(repeat).click()
+        }, 2000);
 
-      // const free = await nightmare
-      //   .wait(4000 + rand(2000))
-      //   .goto('https://www.spotify.com/dk/account/overview/')
-      //   .wait('#js-navbar')
-      //   .wait(4000 + rand(2000))
-      //   .evaluate(() => {
-      //     return /free/.test($('.subscription-status').html())
-      //   })
+        return
+      })
 
-      // if (free) {
-      //   throw 'getout'
-      // }
+    if (errorhtml) {
+      // fs.writeFile(account + '-error.txt', errorhtml, function (err) {
+      //   if (err) return console.log(err);
+      // });
 
-      accountsValid.push(account)
-      if (accountsValid.length === 1 || accountsValid.length % 10 === 0) {
-        // console.log('total ' + accountsValid.length)
+      throw {
+        code: -7
       }
+    }
+    else {
+      processing = false
+    }
 
-      let noplay = await nightmare
-        .wait(4000 + rand(2000))
-        .goto(nAl)
-        .wait(4000 + rand(2000))
+    let change = 0
+    let pause = rand(5) + 2
 
-      const errorhtml = await nightmare
-        // .click(playBtn)
-        .wait(4000 + rand(2000))
-        .evaluate(() => {
-          let shuffle = '.spoticon-shuffle-16:not(.control-button--active)'
-          let repeat = '.spoticon-repeat-16:not(.control-button--active)'
-          let playBtn = '.tracklist-play-pause.tracklist-middle-align'
+    inter = setInterval(async () => {
+      try {
+        let aUrl = album()
 
-          if (!document.querySelector(playBtn)) {
-            return document.querySelector('body').innerHTML
-          }
+        while (aUrl === nAl) {
+          aUrl = album()
+        }
 
-          document.querySelector(playBtn) && document.querySelector(playBtn).click()
-          setTimeout(() => {
-            document.querySelector(shuffle) && document.querySelector(shuffle).click()
-          }, 1000);
-          setTimeout(() => {
-            document.querySelector(repeat) && document.querySelector(repeat).click()
-          }, 2000);
+        nAl = aUrl
+        // console.log('change : ' + nAl)
+        let interalready = await nightmare
+          .goto(nAl)
+          .wait(4000 + rand(2000))
+        // .evaluate(() => {
+        //   return document.querySelector('.connect-bar')
+        // })
 
+        // if (interalready) {
+        //   throw {
+        //     code: 1
+        //   }
+        // }
+
+        if (++change > pause) {
+          change = 0
+          pause = rand(4) + 2
+          console.log(account, 'change pause')
           return
-        })
+        }
 
-      if (errorhtml) {
-        // fs.writeFile(account + '-error.txt', errorhtml, function (err) {
-        //   if (err) return console.log(err);
-        // });
+        await nightmare
+          .click(playBtn)
 
-        throw {
-          code: -7
+        console.log(login + ' change ok ' + change + '/' + pause + ' : ' + total)
+      }
+      catch (e) {
+        console.log('loop error ' + login + ' ' + e.code)
+        if (e.code !== -7) {
+          accountsValid = accountsValid.filter(a => a !== account)
+          clearInterval(inter)
+          await nightmare.end()
+          processing = false
         }
       }
+    }, 1000 * 60 * 10 + rand(1000 * 60 * 5));
 
-      let change = 0
-      let pause = rand(5) + 2
+    // setTimeout(async () => {
+    // console.log('out : ' + account)
+    // clearInterval(inter)
+    // await nightmare.end()
+    // accounts.push(account)
+    // main(true)
+    // }, 1000 * 60 * 60 + rand(1000 * 60 * 60));
 
-      inter = setInterval(async () => {
-        try {
-          let aUrl = album()
-
-          while (aUrl === nAl) {
-            aUrl = album()
-          }
-
-          nAl = aUrl
-          // console.log('change : ' + nAl)
-          let interalready = await nightmare
-            .goto(nAl)
-            .wait(4000 + rand(2000))
-          // .evaluate(() => {
-          //   return document.querySelector('.connect-bar')
-          // })
-
-          // if (interalready) {
-          //   throw {
-          //     code: 1
-          //   }
-          // }
-
-          if (++change > pause) {
-            change = 0
-            pause = rand(4) + 2
-            console.log(account, 'change pause')
-            return
-          }
-
-          await nightmare
-            .click(playBtn)
-
-          console.log(login + ' change ok ' + change + '/' + pause + ' : ' + total)
-        }
-        catch (e) {
-          console.log('loop error ' + login + ' ' + e.code)
-          if (e.code !== -7) {
-            accountsValid = accountsValid.filter(a => a !== account)
-            clearInterval(inter)
-            await nightmare.end()
-            processing = false
-          }
-        }
-      }, 1000 * 60 * 10 + rand(1000 * 60 * 5));
-
-      // setTimeout(async () => {
-      // console.log('out : ' + account)
-      // clearInterval(inter)
-      // await nightmare.end()
-      // accounts.push(account)
-      // main(true)
-      // }, 1000 * 60 * 60 + rand(1000 * 60 * 60));
-
-      console.log('ok ' + login)
-      processing = false
-      anticaptcha()
+    console.log('ok ' + login)
+  }
+  catch (e) {
+    console.log('error ' + login)
+    if (e.code === -7) {
+      accounts.unshift(account)
     }
-    catch (e) {
-      console.log('error ' + login)
-      if (e.code === -7) {
-        accounts.unshift(account)
-      }
-      else if (e.code !== -1) {
-        console.log(e.code)
-      }
-      accountsValid = accountsValid.filter(a => a !== account)
-      await nightmare.end()
-      processing = false
+    else if (e.code !== -1) {
+      console.log(e.code)
     }
-  }, restart ? rand(1000 * 60 * 60) : 0);
+    accountsValid = accountsValid.filter(a => a !== account)
+    await nightmare.end()
+    processing = false
+  }
 }
 
 fs.readFile('spotifyAccount.txt', 'utf8', function (err, data) {
   if (err) return console.log(err);
   accounts = data.split(',')
   // console.log(accounts)
-  anticaptcha()
 });
 
 setInterval(() => {
   if (accounts.length && !processing && !onecaptcha) {
-    // anticaptcha()
+    anticaptcha()
   }
 }, 1000 * 30)
 
