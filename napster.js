8b9e7d9b70a00ef1363279b752a90953a469f534
ASSOUNE Mike <massoune@nexity.fr>
2018-10-25 13:19:53 +0200

    c

--- napster.js
+++ napster.js
@@ -184,7 +184,23 @@ const main = async (restartAccount, persist) => {
       await nightmare.end()
     }
     else {
-      setTimeout(async () => {
+      let inter = setInterval(() => {
+        if (over) { return clearInterval(inter) }
+        await nightmare
+          .goto(album())
+          .wait(2000 + rand(2000))
+          .click(playBtn)
+          .wait(2000 + rand(2000))
+          .click(shuffle)
+          .then()
+          .catch(async (e) => {
+            console.log('catch album' + e)
+            errorLog = true
+          })
+      }, 1000 * 60 * (2 + rand(8)));
+      let time = setTimeout(async () => {
+        if (over) { return clearInterval(time) }
+        clearInterval(inter)
         await nightmare.end(() => {
           main(account)
         })
