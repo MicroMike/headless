92bd6e665411c0889f1a0238d1d14d2cc602c979
ASSOUNE Mike <massoune@nexity.fr>
2018-11-13 16:42:44 +0100

    c

--- napster.js
+++ napster.js
@@ -306,9 +306,9 @@ const main = async (restartAccount, night, timeout) => {
 
           setTimeout(async () => {
             await nightmare
-              .click('.player-play-button .icon-next2')
-              // .wait(2000 + rand(2000))
-              // .click('.player-play-button .icon-play-button')
+              .click(t1 === '0%' ? '.player-play-button .icon-pause2' : '.player-play-button .icon-next2')
+              .wait(2000 + rand(2000))
+              .click(t1 === '0%' ? '.player-play-button .icon-play-button' : 'body')
               .then()
               .catch(async (e) => {
                 clearInterval(inter)
