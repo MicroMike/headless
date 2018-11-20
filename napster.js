c592a9bdee6124fcd1f987097511f2d5b24f2a62
ASSOUNE Mike <massoune@nexity.fr>
2018-11-20 15:30:21 +0100

    c

--- napster.js
+++ napster.js
@@ -419,7 +419,7 @@ const mainInter = setInterval(() => {
 fs.readFile('napsterAccount.txt', 'utf8', function (err, data) {
   if (err) return console.log(err);
   accounts = data.split(',')
-  accounts = shuffle(accounts)
+  // accounts = shuffle(accounts)
   console.log(accounts.length)
   main()
 });
