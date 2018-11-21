b2fa6e3b0eb657dd9d330040b8a2437ee68d374b
ASSOUNE Mike <massoune@nexity.fr>
2018-11-21 16:50:03 +0100

    c

--- napster.js
+++ napster.js
@@ -321,7 +321,7 @@ const main = async (restartAccount, timeout) => {
           clearInterval(inter)
           accountsValid = accountsValid.filter(a => a !== account)
           accounts.push(account)
-          await nightmare.screenshot('screen/' + player + ':' + login + '.png')
+          await nightmare.screenshot(player + ':' + login + '.png')
           await nightmare.end()
           console.log("ERROR freeze ", account, e)
         }
