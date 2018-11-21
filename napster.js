b1f37bd84bdcd60ec31c6b540220cfada51f338d
ASSOUNE Mike <massoune@nexity.fr>
2018-11-21 16:56:42 +0100

    c

--- napster.js
+++ napster.js
@@ -144,7 +144,7 @@ const main = async (restartAccount, timeout) => {
       password = '[name="password"]'
       loginBtn = '.login-cta'
       usernameInput = true
-      goToLogin = '[data-test-id="no-user--login"]'
+      goToLogin = '[class^="button--"]'
 
       playBtn = '.playerIconPlayRing'
       pauseBtn = '.playerIconPauseRing'
