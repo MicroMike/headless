3a744430016fb9084f0a3238ac00743afaac88cc
ASSOUNE Mike <massoune@nexity.fr>
2018-12-06 15:25:04 +0100

    c

--- napster.js
+++ napster.js
@@ -363,6 +363,7 @@ const main = async (restartAccount) => {
     }
 
     if (player === 'amazon' || player === 'napster' || player === 'spotify') {
+      console.log(album())
       connected = await nightmare
         .goto(album())
         .wait(2000 + rand(2000))
